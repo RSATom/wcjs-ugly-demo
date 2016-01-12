d3c5d3ec0a66bc7d5a61331dffa36ff93d4d76ea
Sergey Radionov <rsatom@gmail.com>
2016-01-12 22:17:44 +0600

    show dev tools in Electron by default

--- electron_main.js
+++ electron_main.js
@@ -3,5 +3,6 @@ var BrowserWindow = require("browser-window");
 
 app.on("ready", function() {
   var win = new BrowserWindow({});
+  win.toggleDevTools();
   win.loadUrl("file://" + __dirname + "/index.html");
 })
