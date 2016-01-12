cbaf458a92499e393e1923a69ea0c78fa2661e53
Sergey Radionov <rsatom@gmail.com>
2016-01-12 22:18:20 +0600

    use Electron v0.33.9 by default

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,6 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="0.35.4"
+set npm_config_wcjs_runtime_version="0.33.9"
+rem set npm_config_wcjs_arch="x64"
 set npm_config_wcjs_arch="ia32"
 
 npm install
