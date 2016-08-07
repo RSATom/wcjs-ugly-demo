e58d58a290e1648c7cc7bf7acafec7e36bd3106b
Evan Alter <evan.alter@gmail.com>
2016-08-07 16:47:04 -0500

    Updating electron to 1.3.3

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="0.33.9"
+set npm_config_wcjs_runtime_version="1.3.3"
 rem set npm_config_wcjs_arch="x64"
 set npm_config_wcjs_arch="ia32"
 
