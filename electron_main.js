0881597164e9778bca4141aed29c460479d1cda8
Evan Alter <evan.alter@gmail.com>
2016-08-07 23:24:50 -0500

    Fixing some stuff

--- electron_main.js
+++ electron_main.js
@@ -1,8 +1,19 @@
-var BrowserWindow = require('electron').BrowserWindow
-var app = require('electron').app
+// compatible with both Electron 0.x and 1.x
+try {
+  var BrowserWindow = require("browser-window");
+} catch {
+  var BrowserWindow = require("electron").BrowserWindow;
+}
+
+// compatible with both Electron 0.x and 1.x
+try {
+  var app = require("app");
+} catch
+  var app = require("electron").app;
+}
 
 app.on("ready", function() {
-  var win = new BrowserWindow()
-  win.toggleDevTools()
-  win.loadURL("file://" + __dirname + "/index.html")
+  var win = new BrowserWindow();
+  win.toggleDevTools();
+  win.loadURL("file://" + __dirname + "/index.html");
 })
