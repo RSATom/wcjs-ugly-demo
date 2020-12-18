6fbfcbc313a1cc4e92e24de8a5225caa9a5d48dc
sergey radionov <rsatom@gmail.com>
2020-12-18 18:56:46 +0700

    win: use 64 bit Electron

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,6 +1,6 @@
 set npm_config_wcjs_runtime="electron"
 set npm_config_wcjs_runtime_version="11.1.0"
-rem set npm_config_wcjs_arch="x64"
-set npm_config_wcjs_arch="ia32"
+set npm_config_wcjs_arch="x64"
+rem set npm_config_wcjs_arch="ia32"
 
 npm install
