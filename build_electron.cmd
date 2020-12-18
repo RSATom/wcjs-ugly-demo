9c334e768b02e42b953900616039fa0a8606dda9
sergey radionov <rsatom@gmail.com>
2020-12-18 18:56:09 +0700

    use latest Electron version

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="1.3.3"
+set npm_config_wcjs_runtime_version="11.1.0"
 rem set npm_config_wcjs_arch="x64"
 set npm_config_wcjs_arch="ia32"
 
--- build_electron.sh
+++ build_electron.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 export npm_config_wcjs_runtime="electron"
-export npm_config_wcjs_runtime_version="11.0.3"
+export npm_config_wcjs_runtime_version="11.1.0"
 
 npm install
