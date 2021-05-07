a64ee22cfb2c129e067de0bfc9696f125ed6ef00
sergey radionov <rsatom@gmail.com>
2021-05-07 18:42:49 +0700

    use latest Electron version

--- build_electron.cmd
+++ build_electron.cmd
@@ -1,5 +1,5 @@
 set npm_config_wcjs_runtime="electron"
-set npm_config_wcjs_runtime_version="11.1.0"
+set npm_config_wcjs_runtime_version="12.0.6"
 set npm_config_wcjs_arch="x64"
 rem set npm_config_wcjs_arch="ia32"
 
